08d1d71e6dff7dc1941f4c7f2bbe645fecf89ac8
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2025-01-17 15:48:35 +0100

    feat: create Appcontants and AppChat components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script>
+
+import AppContacts from './components/AppContacts.vue';
+import AppChat from './components/AppChat.vue';
+
 export default {
+  components: {
+    AppContacts,
+    AppChat
+  },
   data() {
     return {
 
@@ -9,6 +17,12 @@ export default {
 </script>
 
 <template>
+  <div class="container">
+    <AppContacts />
+    <AppChat />
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@use "./styles/general.scss"
+</style>
