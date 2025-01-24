1ad1e7f894721d14c62fcf405c6076bd7d254296
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2025-01-24 17:24:01 +0100

    build: istall axios and dotenv dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
+const axios = require('axios');
+
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+
+    axios.get('/user?ID=12345')
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
